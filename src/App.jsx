109425d5d77ada38adf2eb5cb46059af7355164c
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-08-31 16:48:20 +0530

    refactor: Update component imports and file structure
    This commit updates the component imports and file structure to match the updated project structure. The Navbar component is moved to the correct directory and the imports are updated accordingly. This ensures logical and consistent organization of components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { Button } from "./components/ui/button";
-import Feed from "./components/Navbar/Feedback/Feed";
-import Home from "./components/Navbar/Feedback/Home";
+import Feed from "./components/Feedback/Feed";
+import Home from "./components/Feedback/Home";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
