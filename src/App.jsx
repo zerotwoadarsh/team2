b35afffc7281567f1a8fc1b3007f8a4fb919a712
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-09-01 05:04:38 +0530

    refactor: Add leaflet.heat dependency for heatmap visualization

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import Feed from "./components/Feedback/Feed";
-import Home from "./components/Feedback/Home";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Analysis from "./components/Analysis/Analysis";
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero1 from './Hero1'
 import Hero2 from './Hero2'
 
+
 const Hero = () => {
   return (
     <div>
