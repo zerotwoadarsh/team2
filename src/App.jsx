a8d7263f3979a4281b2cbfe7695cc6f0cf111310
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-09-01 03:06:54 +0530

    feat: Add Hero component for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import "./App.css";
 import { Button } from "./components/ui/button";
 import Feed from "./components/Feedback/Feed";
 import Home from "./components/Feedback/Home";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Analysis from "./components/Analysis/Analysis";
 import Post from "./components/Feedback/post"
+
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Hero />} />
+        <Route path="/feed" element={<Feed />} />
         <Route path="/post" element={<Post />} />
         <Route path="/analysis" element={<Analysis />} />
       </Routes>
