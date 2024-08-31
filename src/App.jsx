e895d501908a0025acb23ba98c924f33bc828084
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-08-31 11:59:46 +0530

    feat: Add App component and styles
    This commit adds the App component and its associated styles. The App component is the main component of the application and renders a Button component. The styles are imported from the App.css file.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+
+function App() {
+
+  return (
+    <div>
+    <Button>ajkfas</Button>
+    </div>
+  )
+}
+
+export default App
