2e495928d6a2d43af5c4399e08a505e0ed928447
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-09-01 21:12:51 +0530

    hero1 fix --get_real_time_feed

--- src/components/Hero/Hero1.jsx
+++ src/components/Hero/Hero1.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Hero1 = () => {
     return (
@@ -6,20 +6,18 @@ const Hero1 = () => {
             <section className="bg-gradient-to-b from-black via-gray-800 to-slate-600 text-white">
                 <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
                     <div className="mx-auto max-w-3xl text-center">
-                        <h1
-                            className="text-white bg-clip-text text-3xl font-extrabold  sm:text-5xl"
-                        >
+                        <h1 className="text-white bg-clip-text text-3xl font-extrabold  sm:text-5xl">
                             Stay Vigilant,
-
                             <span className="sm:block">Stay Secure</span>
                         </h1>
 
                         <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-                         Your Ultimate Cybersecurity Companion.
+                            Your Ultimate Cybersecurity Companion.
                         </p>
 
                         <p className="mx-auto mt-2  sm:text-xl/relaxed">
-                        Guarding Your Digital World: Proactive Protection with Cyber Watch.
+                            Guarding Your Digital World: Proactive Protection
+                            with Cyber Watch.
                         </p>
                         <div className="mt-8 flex flex-wrap justify-center gap-4">
                             <a
@@ -27,24 +25,22 @@ const Hero1 = () => {
                                 href="https://forms.gle/cctVEdKLeTwANULo7"
                                 target="_blank"
                             >
-                            Report a cyber crime
+                                Report a cyber crime
                             </a>
 
                             <a
                                 className="block w-full rounded border border-green-600 px-12 py-3 text-sm font-medium text-white hover:bg-green-600 focus:outline-none focus:ring active:bg-green-500 sm:w-auto"
-                                href="#"
+                                href="/feed"
                             >
-                                Cyber crime in your area 
+                                Get Real Time Feed
                             </a>
                         </div>
                     </div>
                 </div>
             </section>
-            <div className=" w-5 bg-white">
-
-            </div>
+            <div className=" w-5 bg-white"></div>
         </div>
-    )
-}
+    );
+};
 
-export default Hero1
+export default Hero1;
